f54d82a9070e6c9209c37f8cc194953af1a56a4b
root <root@localhost.localdomain>
2018-02-15 18:43:10 +0100

    .

--- librerias/ficheros.py
+++ librerias/ficheros.py
@@ -8,4 +8,22 @@ def creadir(dir):
 		print 'el directorio ya existe'	
 	else:
 		os.mkdir(dir)
-		print 'directorio ' (dir)+str 'creado'
+#		print 'directorio ' (dir)+str 'creado'
+
+
+#que nos devuelva user y path
+
+def entorno():
+	for variable, valor in os.environ.iteritems():
+		if variable=='USER' or variable=='PATH':
+			print "%s: %s" % (variable, valor)
+
+#funcion que recupera los ficheros que ocupan ms de
+#os.getsize
+#os.listdir
+def gordos(dir,size):
+	for x in os.listdir(dir):
+		tamano=os.path.getsize(x)
+		if tamano > (size):
+			print x
+				
